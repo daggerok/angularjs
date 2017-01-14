b847f93bc2488bb26e718ec3b9d1206eb18ebbe8
Maksim Kostromin <daggerok@gmail.com>
2017-01-14 21:06:45 +0200

    update webpack

--- config/webpack/prod.babel.js
+++ config/webpack/prod.babel.js
@@ -1,14 +1,21 @@
 import { DefinePlugin } from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { BaseHrefWebpackPlugin } from 'base-href-webpack-plugin';
 
 import config from './common/config.babel';
 import htmlConfig from './common/html.config.babel';
 
-config.devtool = '#source-map';
+if (process.env.NODE_ENV === 'ghpages') {
+  config.output.publicPath = '/angularjs/';
+}
 
+config.devtool = '#source-map';
 config.plugins = [
   ...config.plugins,
   new HtmlWebpackPlugin(htmlConfig),
+  new BaseHrefWebpackPlugin({
+    baseHref: config.output.publicPath,
+  }),
   new DefinePlugin({
     'process.env': {
       'NODE_ENV': JSON.stringify('production')
