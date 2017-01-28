c6515688c182d1b3be8f154616c4ad23c82cfd53
Maksim Kostromin <daggerok@gmail.com>
2017-01-28 20:59:59 +0200

    add few more tests

--- e2e/app.test.js
+++ e2e/app.test.js
@@ -16,16 +16,39 @@ describe('home page', () => {
       footer = browser.element(by.css('footer'))
     });
 
-    it('should have footer', () => {
+    it('should be presented', () => {
       expect(footer.isPresent()).toEqual(true);
     });
 
-    it('footer has correct text', () => {
+    it('should have correct text', () => {
       // everything is a promise:
       footer.getText()
         .then(text => expect(text).toEqual('footer'));
       // but jasmine can handle promises as regular string
       expect(footer.getText()).toEqual('footer');
+    });
+
+    describe('position', () => {
+
+      it('should have correct location', () => {
+        footer.getLocation().then(location => {
+          const { x, y } = location;
+
+          console.log(`x: ${x}, y: ${y}`);
+          expect(x).toEqual(0);
+          expect(y).toEqual(40);
+        });
+      });
+
+      it('should have correct size', () => {
+        footer.getSize().then(size => {
+          const { width, height } = size;
+
+          console.log(`width: ${width}, height: ${height}`);
+          expect(width).toEqual(925);
+          expect(height).toEqual(20);
+        });
+      });
     });
   });
 });
